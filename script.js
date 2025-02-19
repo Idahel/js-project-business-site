25bb60c5df89fc78e1f4e5460e311bcb0e0bbf5e
Ida Hellgren <idakatrinhellgren@gmail.com>
2025-02-19 16:35:55 +0100

    working hamburger menu through javascript, added a footer and some extra styling to the food cards

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const contactWay1 = document.getElementById('contact-way-1')
+const contactWay2 = document.getElementById('contact-way-2')
+const contactWay3 = document.getElementById('contact-way-3')
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+// Toggle menu when hamburger is clicked
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+});
+
+// Close menu when a nav link is clicked
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active')
+        navMenu.classList.remove('active')
+    })
 })
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+contactWay1.addEventListener('click', () => contactWay1.classList.toggle('open'))
+contactWay2.addEventListener('click', () => contactWay2.classList.toggle('open'))
+contactWay3.addEventListener('click', () => contactWay3.classList.toggle('open'))
